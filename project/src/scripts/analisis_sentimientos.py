adaf639a73039826bcf696bb5620448d730c6db6
joorgema <86876791+JrgeMA@users.noreply.github.com>
2023-11-14 14:18:16 +0100

    requirements

--- project/src/scripts/analisis_sentimientos.py
+++ project/src/scripts/analisis_sentimientos.py
@@ -8,6 +8,7 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 translate_client = translate.Client.from_service_account_json('tfgtwitter-735690f3c7f2.json')
 analyzer = SentimentIntensityAnalyzer()
 
+
 def analisis_sentimientos(file_content):
     js_code = file_content.replace('window.YTD.tweets.part0 = ', '')
 
@@ -16,7 +17,7 @@ def analisis_sentimientos(file_content):
     data = json_normalize(json_dat)
 
     df = pd.DataFrame(data['tweet.full_text'])
-    df = df.head(20)
+    df = df.head(5)
 
     # Split the dataframe (tweets with rts and without)
     df_contiene_rts = df[df["tweet.full_text"].str.contains("RT")]
@@ -30,7 +31,6 @@ def analisis_sentimientos(file_content):
     df_sin_rts = df_sin_rts[df_sin_rts["tweet.full_text"] != '']
     df_contiene_rts = df_contiene_rts[df_contiene_rts["tweet.full_text"] != '']
 
-    
     # Get polarity with Vader
     df_contiene_rts['tweet.polarity'] = df_contiene_rts['tweet.full_text'].apply(get_polarity)
     df_sin_rts['tweet.polarity'] = df_sin_rts['tweet.full_text'].apply(get_polarity)
@@ -38,7 +38,8 @@ def analisis_sentimientos(file_content):
     df_sin_rts = df_sin_rts.groupby('tweet.polarity').size().reset_index(name='quantity')
     df_contiene_rts = df_contiene_rts.groupby('tweet.polarity').size().reset_index(name='quantity')
 
-    return df_contiene_rts.sort_values(by=['quantity'], ascending=False), df_sin_rts.sort_values(by=['quantity'], ascending=False)
+    return df_contiene_rts.sort_values(by=['quantity'], ascending=False), df_sin_rts.sort_values(by=['quantity'],
+                                                                                                 ascending=False)
 
 
 def get_polarity(text):
@@ -49,6 +50,5 @@ def get_polarity(text):
         return "Sentimiento Positivo"
     elif -0.05 < compound_english < 0.05:
         return "Sentimiento Neutral"
-    else: return "Sentimiento Negativo"
-
-
+    else:
+        return "Sentimiento Negativo"
