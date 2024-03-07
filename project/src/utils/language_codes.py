3923230837d26de9f6ddc62842a0104c556d2a1b
joorgema <86876791+JrgeMA@users.noreply.github.com>
2024-03-07 16:49:21 +0100

    eliminadas cosas inutiles

--- project/src/utils/language_codes.py
+++
@@ -1,9 +0,0 @@
-language_codes = {'af': 'Africano', 'ar': 'Árabe', 'bg': 'Búlgaro', 'bn': 'Bengalí', 'ca': 'Catalán', 'cs': 'Checo',
-                  'cy': 'Galés', 'da': 'Danés', 'de': 'Alemán', 'el': 'Griego', 'en': 'Inglés', 'es': 'Español',
-                  'et': 'Estonio', 'fa': 'Persa', 'fi': 'Finés', 'fr': 'Francés', 'gu': 'Gujarati', 'he': 'Hebreo',
-                  'hr': 'Croata', 'hu': 'Húngaro', 'id': 'Indonedsio', 'it': 'Italiano', 'ja': 'Japonés',
-                  'kn': 'Kannada', 'ko': 'Coreano', 'lt': 'Lituano', 'lv': 'Letón', 'mk': 'Macedonio',
-                  'ml': 'Malasio', 'mr': 'Marathi', 'ne': 'Nepalí', 'no': 'Noruego', 'pl': 'Polaco',
-                  'pt': 'Portugués', 'ro': 'Rumano', 'sk': 'Eslovaco', 'sl': 'Esloveno', 'so': 'Somalí',
-                  'sq': 'Albanés', 'sv': 'Sueco', 'sw': 'Suajili', 'ta': 'Tamil', 'te': 'Telugu', 'th': 'Tailandés',
-                  'tl': 'Tagalo', 'tr': 'Turco', 'vi': 'Vietnamita'}
